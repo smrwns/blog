3a3ba3ef9ead564e13f8e076ce142ca8bf562d87
smrwns <smrwns@naver.com>
2014-08-17 00:06:29 +0900

    AOP 라이브러리 추가 및 활성화 서블릿 버전 다운그레이드 CAPTCHA 라이브러리 추가

--- src/main/java/com/smrwns/config/RootConfig.java
+++ src/main/java/com/smrwns/config/RootConfig.java
@@ -2,9 +2,11 @@ package com.smrwns.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Controller;
 
 @Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @ComponentScan(basePackages="com.smrwns", excludeFilters ={@ComponentScan.Filter(Configuration.class), @ComponentScan.Filter(Controller.class)})
 public class RootConfig {
 
